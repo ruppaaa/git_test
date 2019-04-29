ec119311a0358e0353ccc3cd69837a5f6fb792a7
admain <admain@qq.com>
2019-04-30 00:14:06 +0800

    修改并推送到远程

--- git/src/cn/itcast/test/GitTest.java
+++ git/src/cn/itcast/test/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello , git");
     }
+    public static void test(){
+        System.out.println("a");
+    }
 }
